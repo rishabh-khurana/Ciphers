91ae18b35aa62ae79a6e34c24e05c8f56e7fe854
Rishabh Khurana <rishabhk994@gmail.com>
2020-03-17 14:17:38 +1100

    row and colum checking

--- utils.py
+++ utils.py
@@ -34,7 +34,32 @@ def GridEncode(First,Second,altGrid):
             if altGrid[i][j] == Second:
                 secPos=[i,j]
                 break
-    
-    secPos[1],firstPos[1]=firstPos[1],secPos[1]
+    # same row         
+    if firstPos[0] is secPos[0]:
+        if (firstPos[1] + 1 == 5):
+            firstPos[1] = 0
+        else: 
+            firstPos[1] = firstPos[1] + 1
+        
+        if (secPos[1] + 1 == 5):
+            secPos[1] = 0
+        else:
+            secPos[1] = secPos[1] + 1
+
+    # same column
+    elif firstPos[1] is secPos[1]:
+        if (firstPos[0] + 1 == 5):
+            firstPos[0] = 0
+        else: 
+            firstPos[0] = firstPos[0] + 1
+        
+        if (secPos[0] + 1 == 5):
+            secPos[0] = 0
+        else:
+            secPos[0] = secPos[0] + 1
+
+    # diff row and column
+    else:
+        secPos[1],firstPos[1]=firstPos[1],secPos[1]
 
     return altGrid[firstPos[0]][firstPos[1]],altGrid[secPos[0]][secPos[1]]
